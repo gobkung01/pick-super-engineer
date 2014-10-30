c4fd8233b14f6ce0a2c7829a56dab66c02cba56f
Teerathash <gobkung01@gmail.com>
2014-10-30 10:35:35 -0300

    add some base code

---
+++ picker.rb
@@ -0,0 +1,26 @@
+#This is funtion to picking up name from file.
+
+def pick_engineer() #Method to pick name from file
+  name_file = File.open("engineers.txt") #Open file text.
+  name_list = Array.new() #Create blank array.
+  name_file.each { |name| name_list.push(name) } #Read name from file then push it to array.
+  rnd_name = name_list[rand(name_list.length)] #Random name from list then print it.
+  puts rnd_name
+  featureA(rnd_name)
+  featureB(rnd_name)
+  featureC(rnd_name , ARGV)
+end
+
+def featureA(name)
+
+end
+
+def featureB(name)
+
+end
+
+def featureC(name , keyw)
+
+end
+
+pick_engineer() #Call method
