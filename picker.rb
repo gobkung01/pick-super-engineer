24ae566136adb2c8108461239cc5b81f8c6f8d76
Teerathash <gobkung01@gmail.com>
2014-11-01 13:02:19 -0300

    program v1.0

--- picker.rb
+++ picker.rb
@@ -5,10 +5,10 @@ def pick_engineer() #Method to pick name from file
   name_list = Array.new() #Create blank array.
   name_file.each { |name| name_list.push(name) } #Read name from file then push it to array.
   rnd_name = name_list[rand(name_list.length)] #Random name from list then print it.
-  puts rnd_name
-  featureA(rnd_name)
-  featureB(rnd_name)
-  featureC(rnd_name , ARGV)
+  puts rnd_name #print random name
+  featureA(rnd_name) #call featureA and send random name
+  featureB(rnd_name) #call featureB and send random name
+  featureC(rnd_name , ARGV) #cal featureC and send random name and keyword
 end
 
 def featureA(name)
@@ -30,10 +30,10 @@ def featureB(name)
 end
 
 def featureC(name , keyw)
-  key = keyw.join
-  if !keyw.empty?
-    if name.include? key
-      puts name
+  key = keyw.join #join array to string
+  if !keyw.empty? #check if not empty
+    if name.include? key #find with keyword
+      puts name #print name to screen
     end
   end
 end
